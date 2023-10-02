93ec78a133bfe17dde56d5a01e2b88f1e663145b
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-10-02 16:59:20 +0200

    Make the searchOnlyItem work with any base url.

--- packages/ui/src/components/Command/SearchOnlyItem.tsx
+++ packages/ui/src/components/Command/SearchOnlyItem.tsx
@@ -1,21 +1,23 @@
 import { CommandItem } from './Command.utils'
 import { useRouter } from 'next/router'
 import { useCommandMenu } from './CommandMenuProvider'
+import { getURL } from 'common'
+import { PropsWithChildren } from 'react'
 
-interface SearchOnlyItemProps {
-  children: React.ReactNode
-  url: string
-}
+interface SearchOnlyItemProps
+  extends PropsWithChildren<{
+    url: string
+  }> {}
 
 export default function SearchOnlyItem({ children, url }: SearchOnlyItemProps) {
   const { project, setIsOpen, setSearch } = useCommandMenu()
   const router = useRouter()
 
-  const baseURL = process.env.NEXT_PUBLIC_SITE_URL
+  const baseUrl = [getURL(), process.env.NEXT_PUBLIC_BASE_PATH].filter(Boolean).join('')
   function handleRouteChange(url: string) {
-    const localizedURL = project?.ref
-      ? `${baseURL}${url.replace('/_/', `/${project?.ref}/`)}`
-      : `${baseURL}${url}`
+    const path = project?.ref ? url.replace('/_/', `/${project?.ref}/`) : url
+
+    const localizedURL = `${baseUrl}${path}`
 
     router.push(localizedURL)
     setIsOpen(false)
